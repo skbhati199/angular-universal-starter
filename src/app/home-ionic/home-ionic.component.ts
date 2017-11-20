670ca4f34ed51f07e8b0bc4ad74c3667697519aa
Gorniv <gornivv@gmail.com>
2017-11-20 19:23:44 +0300

    fix styles

--- src/app/home-ionic/home-ionic.component.ts
+++ src/app/home-ionic/home-ionic.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-ionic',
   templateUrl: './home-ionic.component.html',
-  styleUrls: ['./home-ionic.component.variables.scss']
+  styleUrls: []
 })
 
 export class HomeIonicComponent implements OnInit {
