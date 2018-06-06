a71e5e8800c5316c4ffc8d8c9af5c75675ddaab9
faraz <skbhati199@gmail.com>
2018-06-06 17:23:17 +0530

    add firebase cloud

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+// angular
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// libs
+import { CookieService } from 'ngx-cookie-service';
+// shared
+import { SharedModule } from '@shared/shared.module';
+// components
+import { AppRoutes } from './app.routing';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'my-app' }),
+    HttpClientModule,
+    RouterModule,
+    AppRoutes,
+    BrowserAnimationsModule,
+    SharedModule.forRoot(),
+  ],
+  declarations: [AppComponent],
+  providers: [CookieService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
